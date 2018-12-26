7acdbc6ebd131976e7ffd307a5cbf46471e2e6ab
AlexandrLivitskiy <alexandr.livitskiy@gmail.com>
2018-12-26 15:26:46 +0200

    First fix: matches fixed and move iframe from html to body

--- content.js
+++ content.js
@@ -2,4 +2,4 @@ document.lastChild.lastChild.style = "display: none";
 var frame = document.createElement("iframe");
 frame.id = "iFrame";
 frame.src = "/";
-document.appendChild(frame);
+document.lastChild.appendChild(frame);
