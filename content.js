7c03d8e1b4ab44650e31760147361ce90d1101b9
alexandr.livitskiy <alexandr.livitskiy@ab-soft.net>
2018-12-26 17:05:05 +0200

    Add FullScreen for ifame and change src

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
 document.lastChild.lastChild.style = "display: none";
 var frame = document.createElement("iframe");
 frame.id = "iFrame";
-frame.src = "/";
+frame.style = "top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: none";
+frame.width = "100%";
+frame.height = "100%";
+frame.setAttribute("frameborder", "0");
+frame.src = "https://www.google.com";
 document.lastChild.appendChild(frame);
