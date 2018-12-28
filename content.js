5314854ac3fb1139b949c8cf60dd66d9c8c5e19e
AlexandrLivitskiy <alexandr.livitskiy@gmail.com>
2018-12-28 13:09:05 +0200

    Hide iFrame and add newBody with test "p" element

--- content.js
+++ content.js
@@ -1,9 +1,18 @@
-document.lastChild.lastChild.style = "display: none";
+document.body.style = "display: none";
 var frame = document.createElement("iframe");
 frame.id = "iFrame";
-frame.style = "top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: none";
+frame.style = "top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: none; display: none";
 frame.width = "100%";
 frame.height = "100%";
 frame.setAttribute("frameborder", "0");
 frame.src = "https://www.google.com";
 document.lastChild.appendChild(frame);
+frame.onload = function() {
+    var body = document.body;
+    if (body) document.lastChild.removeChild(body);
+    var newBody = document.createElement("body");
+    var elem = document.createElement("p");
+    elem.innerText = "Hi Lev!";
+    newBody.appendChild(elem);
+    document.lastChild.appendChild(newBody);
+}
