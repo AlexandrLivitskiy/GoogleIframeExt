f7a471f2d8389a7c8fb8645f59dafe8d2ab2f805
AlexandrLivitskiy <alexandr.livitskiy@gmail.com>
2019-09-02 10:50:44 +0300

    Hot Fix

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var offers = document.getElementsByClassName("offer-wrapper");
     for (var i = 0; i < offers.length; i++) {
-        var link = offers[i].getElementsByTagName("a")[1].href.split("#")[0];
+        var link = offers[i].getElementsByTagName("a")[0].href.split("#")[0];
         var p = offers[i].getElementsByTagName("tr")[1].getElementsByClassName("lheight16")[0];
         addViewsCount(p, link);
     }
@@ -12,7 +12,7 @@ function addViewsCount(el, uri) {
     xhr.open('GET', uri, true);
     xhr.onreadystatechange = function() {
         if (this.readyState != 4) return;
-        var views = this.response.split("Перегляди:<strong>")[1].split("</strong>")[0];
+        var views = this.response.split(":<strong>")[1].split("</strong>")[0];
         el.innerHTML = el.innerHTML + "<font color=\"red\"> Просмотров: " + views + "</font>";
         console.log(views);
     };
